50b514fa4fb485de25f9f3bae38e3bd101625f50
Mind <mindkriauciunas@gmail.com>
2021-07-07 00:49:29 +0300

    Header init, mobile friendly

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,7 +1,91 @@
+import {
+    Container,
+    Grid,
+    Hidden,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Toolbar,
+} from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
-import React from "react";
+import { Link as RouterDom, NavLink } from "react-router-dom";
+//RouterDom must not be removed even if not used,without it component doesn't recognize "activeClassName"
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const Header = () => {
-    return <AppBar></AppBar>>;
+    return (
+        <AppBar position="static" className="appBar">
+            <Toolbar className="appBar">
+                <Container maxWidth="lg" className="appBar">
+                    <Grid container alignItems="center" className="appBar">
+                        <List component="nav" className="appBar">
+                            <Grid
+                                container
+                                justify="space-between"
+                                className="appBar"
+                                alignItems="center"
+                            >
+                                <Grid item>
+                                    <ListItem
+                                        component={NavLink}
+                                        to="/"
+                                        className=""
+                                    >
+                                        <img
+                                            src="../Images/Goo.svg"
+                                            className="logoImg"
+                                            alt="Logo"
+                                        />
+                                    </ListItem>
+                                </Grid>
+                                <Hidden smUp>
+                                    <Grid item sm>
+                                        <IconButton edge="end" color="inherit">
+                                            <MenuIcon />
+                                        </IconButton>
+                                    </Grid>
+                                </Hidden>
+                                <Hidden xsDown>
+                                    <Grid item>
+                                        <ListItem
+                                            component={NavLink}
+                                            to=""
+                                            activeClassName="activeLink"
+                                            className="navText"
+                                        >
+                                            <ListItemText primary="Stuff$" />
+                                        </ListItem>
+                                    </Grid>
+                                    <Grid item>
+                                        <ListItem
+                                            component={NavLink}
+                                            to=""
+                                            activeClassName="activeLink"
+                                            className="navText"
+                                        >
+                                            <ListItemText primary="Stuff$" />
+                                        </ListItem>
+                                    </Grid>
+                                    <Grid item>
+                                        <ListItem
+                                            component={NavLink}
+                                            to=""
+                                            activeClassName="activeLink"
+                                            className="navText"
+                                        >
+                                            <ListItemText primary="Stuff$" />
+                                        </ListItem>
+                                    </Grid>
+                                </Hidden>
+                            </Grid>
+                        </List>
+                    </Grid>
+                </Container>
+            </Toolbar>
+        </AppBar>
+    );
 };
+
 export default Header;
